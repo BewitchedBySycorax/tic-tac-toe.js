fa223dee391a972967780148bcc5132c7ead0bbb
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-08-30 18:14:18 +0300

    'setEvents' method implemented.

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,6 +1,21 @@
-﻿var game = {
-	run() {
-		console.log('run!');
+﻿"use strict";
+
+var game = {
+	board: [
+		[false, false, false],
+		[false, false, false],
+		[false, false, false],
+	],
+	setEvents: function() {
+		var td = document.getElementsByTagName('td');
+
+		for ( var i = 0; i < td.length; i++ ) {
+			td[i].addEventListener('click', this.onClick.bind(this));
+		}
+	},
+	run: function() {
+		console.log(this.board);
+		this.setEvents();
 	}
 };
 
